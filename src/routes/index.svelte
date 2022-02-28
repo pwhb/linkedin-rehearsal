8123cb72c0b473c6597cf74a103437188137ed21
pwhb <pwhbdev@gmail.com>
2022-02-28 23:43:02 +0630

    added a search bar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,13 +19,30 @@
 
 <script>
 	export let list;
+	let searchTerm = '';
+	let filteredList;
+
+	$: {
+		filteredList = searchTerm
+			? list.filter((val) => val.title.toLowerCase().includes(searchTerm.toLowerCase()))
+			: list;
+	}
 </script>
 
 <svelte:head>
 	<title>Linkedin Rehearsal</title>
 </svelte:head>
-<div class="grid-cols-2 md:grid grid-cols-5 grid-flow-row gap-4 mb-24">
-	{#each list as { testName, title }}
+<div class="m-8">
+	<input
+		type="text"
+		placeholder="Search Test"
+		class="border rounded rounded-lg py-2 px-4 w-full"
+		bind:value={searchTerm}
+	/>
+</div>
+
+<div class="grid grid-cols-2 px-3  md:grid-cols-5 gap-4 mb-24">
+	{#each filteredList as { testName, title }}
 		<div
 			class="bg-sky-700/75 w-56 h-16 flex flex-col justify-center text-white text-center p-2 mx-auto rounded rounded-lg"
 		>
